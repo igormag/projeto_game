2f7d9c081f3a7a59c396f97bccdab4eb46d63b89
Igor Magalhães <igormag@gmail.com>
2018-07-04 12:02:11 -0300

    Primeiro commit

---
+++ js/transicao.js
@@ -0,0 +1,171 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+// symbols:
+
+
+
+(lib.Interpolar11 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Camada 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#043A67").s().p("A/2XkYAcvcHjuWMepHYMcpHP/i1O1ESMgN1Av8g");
+	this.shape.setTransform(543.8,752.5,4.707,4.707,0,0,0,115.4,159.7);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-958.9,-752.6,1917.9,1505.3);
+
+
+(lib.Interpolar10 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Camada 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#033358").s().p("A/2Y9YAAvrHXuxMipbYMhpcQPi/PEEWMgN0Av8g");
+	this.shape.setTransform(396.1,547.6,3.425,3.425,0,0,0,115.5,159.7);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-698,-547.7,1396,1095.4);
+
+
+(lib.Interpolar9 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Camada 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#052846").s().p("EghJAWKYA4vqIMuUNCouYNCotQYiFOzFLMgQfAvGg");
+	this.shape.setTransform(215.4,322.5,2.017,2.017,0,0,0,106.7,159.7);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-427.9,-322.5,856,645);
+
+
+(lib.Interpolar8 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Camada 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#05233C").s().p("EgqcATYYCKzEM3wISHmVYSFmVUIEmNkNjMgjTAjSg");
+	this.shape.setTransform(0,-0.1);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-271.6,-159.6,543.3,319.3);
+
+
+// stage content:
+(lib.transicao = function(mode,startPosition,loop) {
+if (loop == null) { loop = false; }	this.initialize(mode,startPosition,loop,{});
+
+	// Camada 6
+	this.instance = new lib.Interpolar8("synched",0);
+	this.instance.parent = this;
+	this.instance.setTransform(1415,970.7,1.143,1.143,110,0,0,40.1,158);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1).to({regX:0,regY:0,rotation:109.7,x:1600.4,y:988.5},0).wait(1).to({rotation:108.9,x:1600.7,y:985.7},0).wait(1).to({rotation:107.3,x:1601,y:980.6},0).wait(1).to({rotation:104.9,x:1601.3,y:972.9},0).wait(1).to({rotation:101.6,x:1601.1,y:962},0).wait(1).to({rotation:97,x:1599.8,y:947.2},0).wait(1).to({rotation:91,x:1596.4,y:928},0).wait(1).to({rotation:83.1,x:1588.7,y:903.4},0).wait(1).to({rotation:72.6,x:1573.6,y:872.9},0).wait(1).to({rotation:58.7,x:1545.5,y:837.6},0).wait(1).to({rotation:40.4,x:1497.1,y:803.4},0).wait(1).to({rotation:19.3,x:1431.5,y:785.1},0).wait(1).to({rotation:4.3,x:1382.8,y:787.2},0).wait(1).to({regX:40.1,regY:158.1,rotation:0,x:1415.1,y:970.7},0).wait(19).to({startPosition:0},0).to({regY:158,rotation:110,x:1415},10).wait(2));
+
+	// Camada 4
+	this.instance_1 = new lib.Interpolar9("synched",0);
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(1417,972.8,1.143,1.143,108.3,0,0,211.5,322.8);
+	this.instance_1._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_1).wait(2).to({_off:false},0).wait(1).to({regX:0.2,regY:0.3,rotation:108,x:1842.1,y:857},0).wait(1).to({rotation:107.2,x:1840.3,y:850.7},0).wait(1).to({rotation:105.6,x:1836.8,y:839.3},0).wait(1).to({rotation:103.3,x:1831,y:822.2},0).wait(1).to({rotation:100,x:1821.7,y:798.7},0).wait(1).to({rotation:95.5,x:1806.9,y:767.7},0).wait(1).to({rotation:89.6,x:1783.6,y:728.5},0).wait(1).to({rotation:81.8,x:1747.1,y:681},0).wait(1).to({rotation:71.5,x:1689.5,y:626.6},0).wait(1).to({rotation:57.8,x:1599.8,y:571.9},0).wait(1).to({rotation:39.7,x:1466.9,y:535},0).wait(1).to({rotation:19,x:1308.6,y:545.7},0).wait(1).to({rotation:4.2,x:1203.1,y:587.5},0).wait(1).to({regX:211.6,regY:322.8,rotation:0,x:1417,y:972.8},0).wait(13).to({startPosition:0},0).to({regX:211.5,rotation:108.3},13).wait(3));
+
+	// Camada 3
+	this.instance_2 = new lib.Interpolar10("synched",0);
+	this.instance_2.parent = this;
+	this.instance_2.setTransform(1416.6,969.8,1.143,1.143,120.1,0,0,391.9,545.3);
+	this.instance_2._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_2).wait(5).to({_off:false},0).wait(1).to({regX:0.5,regY:0.6,rotation:119.8,x:2178.9,y:890.4},0).wait(1).to({rotation:118.8,x:2177.5,y:877.7},0).wait(1).to({rotation:117.1,x:2174.4,y:855.1},0).wait(1).to({rotation:114.5,x:2168.4,y:820.8},0).wait(1).to({rotation:110.8,x:2157.4,y:773.1},0).wait(1).to({rotation:105.9,x:2137.6,y:709.9},0).wait(1).to({rotation:99.3,x:2103.1,y:629},0).wait(1).to({rotation:90.6,x:2044,y:529.6},0).wait(1).to({rotation:79.2,x:1944.5,y:414},0).wait(1).to({rotation:64,x:1780.4,y:295.1},0).wait(1).to({rotation:44.1,x:1527.9,y:211.4},0).wait(1).to({rotation:21.1,x:1223,y:228.1},0).wait(1).to({rotation:4.7,x:1021.4,y:313},0).wait(1).to({regX:391.9,regY:545.4,rotation:0,x:1416.7,y:969.9},0).wait(7).to({startPosition:0},0).to({regY:545.3,rotation:120.1,x:1416.6,y:969.8},15).wait(4));
+
+	// Camada 1
+	this.instance_3 = new lib.Interpolar11("synched",0);
+	this.instance_3.parent = this;
+	this.instance_3.setTransform(1420.7,972.2,1.143,1.143,120.1,0,0,543.1,752.4);
+	this.instance_3._off = true;
+
+	this.timeline.addTween(cjs.Tween.get(this.instance_3).wait(9).to({_off:false},0).wait(1).to({regX:0.6,regY:0.8,rotation:119.7,x:2473.9,y:859.4},0).wait(1).to({rotation:118.6,x:2471.5,y:838.9},0).wait(1).to({rotation:116.6,x:2466.2,y:802},0).wait(1).to({rotation:113.5,x:2455.5,y:746},0).wait(1).to({rotation:109.1,x:2435.1,y:667.3},0).wait(1).to({rotation:103.1,x:2397.5,y:562.5},0).wait(1).to({rotation:94.9,x:2329.8,y:428.5},0).wait(1).to({rotation:84,x:2210,y:265.9},0).wait(1).to({rotation:69.2,x:2003.1,y:87.4},0).wait(1).to({rotation:49.1,x:1663.9,y:-58.7},0).wait(1).to({rotation:24.5,x:1212.3,y:-66.3},0).wait(1).to({rotation:5.5,x:885.6,y:58},0).wait(1).to({regX:543.2,regY:752.5,rotation:0,x:1420.7,y:972.2},0).wait(2).to({startPosition:0},0).to({regX:543.1,regY:752.4,rotation:120.1},16).wait(5));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(2131,1098.9,365.8,658.1);
+// library properties:
+lib.properties = {
+	id: '73DB814E84F5E94988EB0461D26423E8',
+	width: 1440,
+	height: 900,
+	fps: 30,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['73DB814E84F5E94988EB0461D26423E8'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
